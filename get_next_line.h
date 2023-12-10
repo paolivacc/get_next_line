7c6a301443f8d94841eee9cbf2b6be18efa39dd9
Susana Paoli Vaccaro Nunez <svaccaro@c2r10s1.42malaga.com>
2023-12-10 12:31:03 +0100

    Add functions to .h file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: svaccaro <svaccaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 17:20:27 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/12/01 11:37:53 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:48:27 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,28 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <unistd,h>
+# include <unistd.h>
 
+BUFFER_SIZE 100
 /**
  * @brief Read a line from fd file and returns it.
  * @param fd file descriptor to read from.
  * @return the line read.
  */
 char	*get_next_line(int fd);
+/**
+ * @brief Computes the length of the string s.
+ * @param s The string to compute the length of.
+ * @return The number of characters that precede the terminating NUL character.
+ */
+size_t  ft_strlen(const char *s);
+/**
+ * @brief Find the first occurrence of c in s.
+ * @param s The string to search.
+ * @param c The character to find.
+ * @return The index of the first occurrence of c in s, or NULL if not found.
+ */
+int	ft_strchr_index(const char *s, int c);
+
 
 #endif /*GET_NEXT_LINE_H*/
