59cbf7b849e742ab1599643919e76c6c230f0705
Susana Paoli Vaccaro Nunez <svaccaro@c2r10s1.42malaga.com>
2023-12-10 12:30:21 +0100

    Create function

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,89 @@
 /*   By: svaccaro <svaccaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 12:48:41 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/12/02 20:27:13 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:49:36 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-crear solo una variable estática
+static int	save_info_read(int fd, ssize_t *save_bytes)
+{
+	int	index;
+	int bytes_read;
+
+	index = NULL;
+	bytes_read = 1;
+	while (!index && bytes_read)
+	{	
+		bytes_read = read(fd, &save_bytes, BUFFER_SIZE);
+		index = ft_strchr_index(save_bytes, '\n');
+	}
+	return (index);
+}
+void	keep_line(char save_bytes, char *line, int index)
+{
+	int	len;
+	
+	len = ft_strlen(save_bytes);
+	ft_
+	
+}
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
+	static char	save_bytes;
+	char				line;
+	int					index;
+	
+	index = save_info_read(fd, &save_bytes);
+	if (!&save_bytes || !index)
+		return (NULL);
+	keep_line(save_bytes, &line, index);
+	update_bytes_read(&save_bytes);
+	return (line);
+}
 
-	str = read_line(); //lee una linea y devuelve esa linea leida
-					   //tener en cuenta stdin y file
-	if (no hay más nada que leer || error)
+
+	
+	//leer y guardar en variable estática lo que leí del archivo
+	//guardar linea
+	//actualizar variable estática
+
+
+
+
+
+
+char	*get_next_line(int fd)
+{
+	static char	*save_bytes;
+	ssize_t			bytes_read;
+	char				*line;
+	char				*tmp;
+	int					index;
+
+	tmp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!tmp)
+		return (NULL);
+	bytes_read = read(fd, tmp, BUFFER_SIZE);
+	if (bytes_read == 0 || bytes_read == -1)
+		return (NULL);
+	if (!save_bytes)
+		save_bytes = strdup(tmp);
+	index = ft_strchr(save_bytes, '\n');
+	if (index)
+	{
+		line = ft_substr(save_bytes, 0, index);
+	}
+	
+	
+		
+	if (ft_strchr(tmp, '\n'))
+	{
+		
+	}
+	if (bytes_read)
 		str = NULL;
 	return (str);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,18 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: svaccaro <svaccaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 13:01:24 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/10/24 13:07:07 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:44:52 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_line()
-{
-	char	*line;
 
-	return (line);
+size_t  ft_strlen(const char *s)
+{
+        size_t  cnt;
+
+        cnt = 0;
+        while (s[cnt] != '\0')
+                cnt++;
+        return (cnt);
+}
+
+int ft_strchr_index(const char *s, int c)
+{
+	int			ptr;
+	unsigned char	cc;
+
+	cc = (unsigned char)c;
+	ptr = 0;
+	while (s[ptr])
+	{
+		if (s[ptr] == cc)
+			return (ptr);
+		ptr++;
+	}
+	if (cc == '\0')
+		return (ptr);
+	return (NULL);
+}
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+        char    *substr;
+        size_t  slen;
+
+        if (!s)
+                return (NULL);
+        slen = ft_strlen(s);
+        if (start >= slen || len == 0)
+                return (ft_strdup(""));
+        if (slen - start < len)
+                len = slen - start;
+        substr = malloc((len + 1) * sizeof(char));
+        if (substr == NULL)
+                return (NULL);
+        ft_strlcpy(substr, s + start, len + 1);
+        return (substr);
 }
